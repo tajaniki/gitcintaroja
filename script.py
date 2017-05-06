0fa286b39ab73be4769a333854e3ffb5102dc9c5
axva <aavm13@gmail.com>
2017-05-05 20:25:42 -0500

    esta linea es mas pro

--- script.py
+++ script.py
@@ -9,3 +9,6 @@ nueva super ultra linea pro xD
 #comentario muy útil para los desarrolladores
 
 linea incluso mas pro que todas las anteriores
+
+
+linea incluso muchisimo mas pro de pros que todas las anteriores ;)
